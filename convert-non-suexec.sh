558d44f56d4bc9cc52e5e3beb0cd7ff736d4a7c1
vaLentin chernoZemski <valentin@siteground.com>
2011-02-16 14:24:33 +0200

    convert-non-suexec.sh - The script has been entirely rewriten. The main logic remains the same. 	The script is now able to convert/restore all users on the server to/from suexec/mod_cgi 	The script now also supports conversion/restoration of a single username on the server. 	The script still needs a bit of optimization and code cleanup.

--- convert-non-suexec.sh
+++ convert-non-suexec.sh
@@ -18,91 +18,131 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 
-VERSION='0.0.3'
+VERSION='1.0.1'
 
 function usage () {
-	echo "Use: $0 [fix|revert]"
+	echo "Use: $0 [fix|revert] [username|all]"
 	echo " $0 fix - This will set the permissions of all clients' files and dirs so they can correctly work with suexec"
 	echo " $0 revert - This will revert clients' files and dirs permissions as they were before the fix option"
+	echo " $0 username - Apply the fix|revert actions only to a specific username's home directory"
+	echo " $0 all - Apply the fix|revert actions to all home directories on this server"
 	exit 1
 }
 
-if [ $# -ne 1 ]; then
+if [ $# -ne 2 ]; then
+	usage
+fi
+if [ "$1" != "fix" ] && [ "$1" != "revert" ]; then
 	usage
 fi
 
-mkdir -p /usr/local/1h/bin/
-target_file='/usr/local/1h/bin/set_no_suexec_perms'
+if [ "$2" != "all" ]; then
+	if [ ! -d /home/$2 ]; then
+		print "Home dir for username $2 (/home/$2) does not exists."
+		exit 1
+	fi
+fi
 
-if [ "$1" == 'fix' ]; then
-	# Fix .htaccess files 
-	for htaccess in $(grep -iH 'ForceType application/x-httpd-php' /home/*/public_html/.htaccess | grep -v \# | cut -d : -f 1); do
-		sed -i '/[Ff][Oo][Rr][Cc][Ee][Tt][Yy][Pp][Ee]/aAddHandler application/x-httpd-php .php .php5 .php4 .php3' "${htaccess}"
-		echo "sed -i '/AddHandler application/D' ${htaccess}" >> $target_file
-	done
+action="$1"
+user="$2"
+changes_storage='converted.by.1h'
 
-	for htaccess in $(grep -iH 'suPHP_ConfigPath' /home/*/public_html/.htaccess | grep -v \# | cut -d : -f 1); do
-		sed -i '/[Ss][Uu][Pp][Hh][Pp]_[Cc][Oo][Nn][Ff][Ii][Gg][Pp][Aa][Tt][Hh]/s/\(^.*$\)/# \1/' "${htaccess}"
-		echo "sed -i '/[Ss][Uu][Pp][Hh][Pp]_[Cc][Oo][Nn][Ff][Ii][Gg][Pp][Aa][Tt][Hh]/s/#//g' $htaccess" >> $target_file
-	done
+function revert_changes {
+	if [ ! -x /home/$1/$changes_storage ]; then
+		return 0
+	fi
+	# Execute /home/$1/changes_storage
+	/home/$1/$changes_storage
+	return 1
+}
 
-    for htaccess in $(grep -iH '^Options' /home/*/public_html/.htaccess | grep -v \# | cut -d : -f 1); do
-		sed -i "/^Options/s/$/ +ExecCGI/" "${htaccess}"
-		echo "sed -i '/^Options/s/+ExecCGI//' $htaccess" >> $target_file
-    done
+function do_changes {
+	do_user="$1"
+	target_file="/home/$do_user/$changes_storage"
 
-	for i in `ls -A1 /var/cpanel/users | grep ^[a-zA-Z]`; do
-		if [ ! -d /home/$i/public_html ]; then
-			continue
+	# Find all .htaccess files in this client's directory and start working on them
+	for htaccess in $(find /home/$do_user/ -name .htaccess); do
+		# Find all lines with ForceType application.* and append addhandler beneath them
+		if ( grep -v \# ${htaccess} | grep -i 'ForceType application/x-httpd-php' >> /dev/null 2>&1 ); then
+			sed -i '/[Ff][Oo][Rr][Cc][Ee][Tt][Yy][Pp][Ee]/aAddHandler application/x-httpd-php .php .php5 .php4 .php3' "${htaccess}"
+			echo "sed -i '/AddHandler application/D' ${htaccess}" >> $target_file
 		fi
+		# Comment out all lines with suPHP_ConfigPath
+		if ( grep -v \# ${htaccess} | grep -i 'suPHP_ConfigPath' >> /dev/null 2>&1 ); then
+			sed -i '/[Ss][Uu][Pp][Hh][Pp]_[Cc][Oo][Nn][Ff][Ii][Gg][Pp][Aa][Tt][Hh]/s/\(^.*$\)/# \1/' "${htaccess}"
+			echo "sed -i '/[Ss][Uu][Pp][Hh][Pp]_[Cc][Oo][Nn][Ff][Ii][Gg][Pp][Aa][Tt][Hh]/s/#//g' $htaccess" >> $target_file
+		fi
+		# Append '+ExecCGI' to all lines which contains Options 
+		if ( grep -i 'Options' ${htaccess} >> /dev/null 2>&1 ) && ( ! grep ExecCGI ${htaccess} >> /dev/null 2>&1 ); then
+			sed -i "/Options/s/$/ +ExecCGI/" "${htaccess}"
+			echo "sed -i '/Options/s/ +ExecCGI//' $htaccess" >> $target_file
+		fi
+		
+		if ( grep -v \# ${htaccess} | grep -Ei 'php_flag|php_value' >> /dev/null 2>&1 ); then
+			php_ini_values=$(grep -Ei 'php_flag|php_value' ${htaccess} | awk '{print $2, "=" ,$3}')
+			# Comment out all php_flags and php_values in this .htaccess
+			sed -i '/\([pP][Hh][Pp]_[Ff][Ll][Aa][Gg]\|[Pp][Hh][Pp]_[Vv][Aa][Ll][Uu][Ee]\)/s/\(^.*$\)/# \1/i' ${htaccess}
+			# Generate full path to a php.ini file from the .htaccess
+			php_ini=$(echo $htaccess | sed 's/.htaccess/php.ini/')
+			# Strip the .htaccess from the the find results so we can generate the dir where it is located
+			dir_ini=$(echo $htaccess | sed 's/.htaccess//')
+			echo "$php_ini_values" >> ${php_ini}
+			chown "${do_user}":"${do_user}" "${php_ini}"
+			# In the directory were we found the .htaccess with the php_flags find all dirs in maxdepth 3 
+			# and copy the newly created php.ini files in them
+			find "${dir_ini}" -maxdepth 3 -type d -exec cp -a "${php_ini}" {} \; >> /dev/null 2>&1
+			# Finally write down the revert process to the $target_file
+			echo "sed -i '/\([pP][Hh][Pp]_[Ff][Ll][Aa][Gg]\|[Pp][Hh][Pp]_[Vv][Aa][Ll][Uu][Ee]\)/s/#//g' ${result}" >> $target_file
+			echo "find '${dir_ini}' -maxdepth 3 -name php.ini -exec rm -f {} \;" >> $target_file
+		fi
+	done
 
-		# Fix ownerships
-		#find /home/$i/public_html ! -user $i -exec sh -c 'ls -al $0 | awk "{print \"chown\",\$3\":\"\$4, \"\\\"\"\$9\"\\\"\"}"' {} \; -exec chown $i: {} \; >> $target_file 2>&1;
-		#find /home/$i/public_html ! -user $i -exec ls -al {} \; | while read perms node owner group size month date hour filename; do
-		find /home/$i/ ! -user $i -printf "chown %u:%g \"%p\"\n" | while read cmd owner fullpath; do
-			temp_path=$(echo $fullpath | sed 's/"//g')
-			chown $i: "${temp_path}"
-			#echo "temp path is '$temp_path'"
-			echo "${cmd} ${owner} ${fullpath}" >> $target_file
-		done
-		# Fix permissions
+	if [ ! -d /home/$do_user/public_html ]; then
+		# this client does not have a public_html dir so we will just go ahead and return from the function
+		return 0
+	fi
 
-		#find /home/$i/public_html \( -perm -000777 -o -perm -000770 -o -name .htaccess \) -print | while read result; do
-		find /home/$i/public_html \( -perm -000777 -o -perm -000770 -o -name .htaccess \) -printf "\"%p\"\n" | while read fullresults; do
-			result=$(echo "$fullresults" | sed 's/"//g')
-			if [[ "$result" =~ ".htaccess" ]]; then
-				flags=$(grep -Ei 'php_flag|php_value' "$result" | grep -v \#)
-				if [ -z "$flags" ]; then
+	# Change the ownership of all files and dirs which are not owned by $do_user to $do_user
+	find /home/$do_user/public_html ! -user $do_user -printf "chown %u:%g \"%p\"\n" | while read cmd owner fullpath; do
+		temp_path=$(echo $fullpath | sed 's/"//g')
+		chown $i: "${temp_path}"
+		echo "${cmd} ${owner} ${fullpath}" >> $target_file
+	done
+
+	# Find all files/dirs writable by group and others
+	find /home/$do_user/public_html \( -perm -000777 -o -perm -000770 \) -printf "%p\n" | while read path; do
+		# Remove this writable bit for group/others from the file/dir
+		chmod go-w "${path}"
+		# Write down the revert process
+		echo "chmod 777 \"${path}\"" >> $target_file
+	done
+}
+
+case "$action" in
+	"fix")
+		if [ "$user" == 'all' ]; then
+			for user in $(ls -A1 /home); do
+				if [ ! -d /home/$user ] || ( ! grep "$user:x:" /etc/passwd >> /dev/null 2>&1 ); then
 					continue
 				fi
-				sed -i '/\([pP][Hh][Pp]_[Ff][Ll][Aa][Gg]\|[Pp][Hh][Pp]_[Vv][Aa][Ll][Uu][Ee]\)/s/\(^.*$\)/# \1/i' $result
-				php_ini="$(echo \"$result\" | sed 's/.htaccess//')php.ini"
-				dir_ini=$(echo "$result" | sed 's/.htaccess//')
-				user=$(ls -al "$result" | awk '{print $3}')
-				echo $flags | awk '{print $2, $3}' | while read option value; do
-					#echo "$result | $option = $value -> $php_ini"
-					echo "$option = $value" >> ${php_ini}
-				done
-				chown "${user}":"${user}" "${php_ini}"
-				find "${dir_ini}" -maxdepth 3 -type d -exec cp -a "${php_ini}" {} \;
-				echo "sed -i '/\([pP][Hh][Pp]_[Ff][Ll][Aa][Gg]\|[Pp][Hh][Pp]_[Vv][Aa][Ll][Uu][Ee]\)/s/#//g' ${result}" >> $target_file
-				echo "find '${dir_ini}' -maxdepth 3 -name php.ini -exec rm -f {} \;" >> $target_file
-			else
-				chmod 755 "${result}"
-				echo "chmod 777 \"${result}\"" >> $target_file
-			fi
-		done
-	done
-	if [ -f $target_file ]; then
-		chmod 700 $target_file
-	fi
-elif [ "$1" == 'revert' ]; then
-	if [ -x $target_file ]; then
-		$target_file
-	else
-		echo "$target_file missing or not executable. nothing to revert then."
-		#exit 1
-	fi
-else
-	usage
-fi
+				do_changes $user
+			done
+		else
+			do_changes $user
+		fi
+	;;
+	"revert")
+		if [ "$user" == 'all' ]; then
+			for user in $(ls -A1 /home); do
+				if [ ! -d /home/$user ] || ( ! grep "$user:x:" /etc/passwd >> /dev/null 2>&1 ); then
+					continue
+				fi
+				revert_changes $user
+			done
+		else
+			revert_changes $user
+		fi
+	;;
+	*)
+		usage
+esac
